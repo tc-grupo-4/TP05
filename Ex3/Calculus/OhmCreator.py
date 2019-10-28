9b001dd8f4303a2ff1a1dea827350f13bf0c8638
Ian Diaz <idiaz@itba.edu.ar>
2019-10-28 18:09:06 -0300

    Pushie hasta el problema del buffer a la entrada

--- Ex3/Calculus/OhmCreator.py
+++ Ex3/Calculus/OhmCreator.py
@@ -6,6 +6,7 @@ parser = sp.SpiceParser()
 freq, mag, phase = parser.parseSpiceFile(filePath)
 
 outF=open("SALIDA.csv","w+")
+outF.write("Frecuency (Hz), Magnitude (Ohms), Phase (°)\n")
 for i in range(0,len(freq)):
     outF.write(str(freq[i])+','+str(10**(mag[i]/20))+','+str(phase[i])+'\n')
 outF.close()
